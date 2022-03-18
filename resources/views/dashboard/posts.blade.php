576c7285ea722e6c06177dcc30ef1165edfbad2c
ElMauriciooo <rodriguezmauricio08@comunidad.unam.mx>
2022-03-17 21:26:18 -0600

    Warnings clase jueves 17

--- resources/views/dashboard/posts.blade.php
+++ resources/views/dashboard/posts.blade.php
@@ -1,28 +1,30 @@
 @extends('layouts.master')
 
-@section('content')
-   <form>
-      <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-        <div id="emailHelp" class="form-text">Well never share your email with anyone else. </div>
-      </div>
-      <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
-      </div>
-      <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-      </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
-    </form> 
+@section('content') 
+@include('fragments.validation-errors')
+  <form method="POST" action="{{ route("post.store") }}">
+    @csrf
+    <div class="mb-3">
+        <label for="tittle">Titulo</label>
+        <input type="text" class="form-control" name="tittle" id="tittle">
+    </div>
 
-    <form action="" method="post">
-      <div class="form-group">
-         <label for="title">Titulo</label>
-         <input type="text" class="form-control" name="title" id="title">
-       </div>
-       
-    </form>
+    <div class="form-group">
+        <label for="url_clean">Url Limpia</label>
+        <input type="text" class="form-control" name="url_clean">
+    </div>
+
+    <div class="form-group">
+        <label for="content">Contenido</label>
+        <h3><textarea name="form-control" id="content" name="content" rows="3"></textarea></h3>
+        
+    </div>
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+
+  </form>
+   
+
+
+
 @endsection
